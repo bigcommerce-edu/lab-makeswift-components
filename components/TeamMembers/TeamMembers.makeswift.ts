27fceae32cc376f8040c2ecfe17b73a5e436665b
Chris Nanninga <chris.nanninga@bigcommerce.com>
2024-06-17 16:44:38 -0500

    Register Team Members component

--- components/TeamMembers/TeamMembers.makeswift.ts
+++ components/TeamMembers/TeamMembers.makeswift.ts
@@ -11,3 +11,14 @@ import {
   TextInput 
 } from '@makeswift/runtime/controls'
 import { runtime } from '@/lib/makeswift/runtime'
+
+runtime.registerComponent(
+  lazy(() => import('./TeamMembers')),
+  {
+    type: 'team-members',
+    label: 'Team Members',
+    props: {
+
+    },
+  }
+)
