23b2276253fdb0e1657001a33f891d35261a76ae
Chris Nanninga <chris.nanninga@bigcommerce.com>
2024-09-18 11:11:14 -0500

    Add List control for team members

--- components/ui/team-members/team-members.makeswift.ts
+++ components/ui/team-members/team-members.makeswift.ts
@@ -20,7 +20,26 @@ runtime.registerComponent(
     type: 'team-members',
     label: 'Team Members',
     props: {
-
+      members: List({
+        label: 'Members',
+        type: Shape({
+          type: {
+            name: TextInput({
+              label: 'Name',
+            }),
+            position: TextInput({
+              label: 'Position',
+            }),
+            image: Image({
+              label: 'Image',
+              format: Image.Format.URL,
+            }),
+          },
+        }),
+        getItemLabel(member) {
+          return member?.name || 'Team Member'
+        },
+      }),
     },
   }
 );
--- components/ui/team-members/team-members.tsx
+++ components/ui/team-members/team-members.tsx
@@ -3,21 +3,40 @@
 import { CSSProperties, forwardRef, ReactNode, Ref, useState } from 'react';
 import { clsx } from 'clsx';
 
-interface Props {
+interface Member {
+  name?: string
+  position?: string
+  image?: string
+}
 
+interface Props {
+  members: Member[]
 }
 
 export const TeamMembers = forwardRef((
   { 
-
+    members,
   }: Props, 
   ref: Ref<HTMLDivElement>
 ) => {
   return (
     <div
+      className={clsx(
+        "w-full"
+      )}
       ref={ref}
     >
-      Team Members
+      
+      {(members.length > 0) ?
+
+      <div className="w-full"><h3 className="text-lg text-center">Number of Team Members: {members.length}</h3></div>
+
+      : 
+
+      <div className="w-full"><h3 className="text-lg text-center">Add a Team Member</h3></div>
+
+      }
+
     </div>
   )
 });
