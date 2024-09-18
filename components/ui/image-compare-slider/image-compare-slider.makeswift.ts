c522c7ddba65b3d93114823f60a6144124abe105
Chris Nanninga <chris.nanninga@bigcommerce.com>
2024-09-18 09:55:19 -0500

    Register Image Compare Slider component

--- components/ui/image-compare-slider/image-compare-slider.makeswift.ts
+++ components/ui/image-compare-slider/image-compare-slider.makeswift.ts
@@ -3,3 +3,14 @@
 import { lazy } from 'react';
 import { Image, Style } from '@makeswift/runtime/controls';
 import { runtime } from '~/lib/makeswift/runtime';
+
+runtime.registerComponent(
+  lazy(() => import('./image-compare-slider')),
+  {
+    type: 'img-compare-slider',
+    label: 'Image Compare Slider',
+    props: {
+
+    },
+  }
+);
--- lib/makeswift/components.ts
+++ lib/makeswift/components.ts
@@ -1,2 +1,3 @@
 import '~/components/ui/accordions/accordion.makeswift';
 import '~/components/ui/tabs/tabs.makeswift';
+import '~/components/ui/image-compare-slider/image-compare-slider.makeswift';
